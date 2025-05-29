1b8be4d130fe0c85f2571cafa19c51808651a4c9
solomonschwartz <sschwar8@mail.yu.edu>
2025-05-29 15:44:29 -0400

    solomon-schwartz-website - v4.0 - netlify stuff for github

--- website-v4.0/vite.config.ts
+++ website-v4.0/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Root-relative paths
+  base: './', // Use relative paths (index.html expects CSS/JS in same folder)
   build: {
     outDir: '../',       // Output directly into Website/
-    emptyOutDir: false   // Prevents deleting the rest of Website/!
+    emptyOutDir: false   // Prevent deletion of parent folder
   },
   plugins: [react()]
 });
