51a2fa4c22e5400fc910ba776fdd4d041f23487a
solomonschwartz <sschwar8@mail.yu.edu>
2025-05-29 16:52:16 -0400

    solomon-schwartz-website - v4.0 - static version - init commit

--- website-v4.0/src/App.tsx
+++ website-v4.0/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { HashRouter, Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Blogs from './pages/Blogs';
 import Videos from './pages/Videos';
@@ -8,7 +8,7 @@ import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
@@ -16,7 +16,7 @@ function App() {
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
